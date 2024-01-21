8e447be04ca9e3e84970ffca1f22cc3dcaef88c2
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-01-21 13:07:13 +0000

    update on basic folder

---
+++ 01_Basic/02_string.js
@@ -0,0 +1,10 @@
+var name="Pratik-Pv"
+var str2="Pratik://origin%50Vishwakarma-lucknow"
+
+
+// console.log(`Hello this is my ${name} and i am study ${4+5} hr video of javascript`);
+console.log(name)
+var newStr=name.slice(-9,3)
+console.log(newStr);
+let secondStr=name.substring(0,5)
+console.log(secondStr)
---
+++ 01_Basic/03_numberAndMath.js
@@ -0,0 +1,11 @@
+const score=500
+
+const balance=new Number(400)// object of numer
+console.log(score);
+console.log(balance);
+
+console.log(typeof(balance.toString()));
+console.log(balance.toFixed(2));
+
+const newnumber=95.656878
+console.log(newnumber.toPrecision(5));
