67c7ab2320213c4fde5c28977a48787294526ec2
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-01-21 13:29:58 +0000

    upadat on 03_numberAndMath file

--- 01_Basic/03_numberAndMath.js
+++ 01_Basic/03_numberAndMath.js
@@ -1,11 +1,34 @@
-const score=500
+// const score=500
 
-const balance=new Number(400)// object of numer
-console.log(score);
-console.log(balance);
+// const balance=new Number(400)// object of numer
+// console.log(score);
+// console.log(balance);
 
-console.log(typeof(balance.toString()));
-console.log(balance.toFixed(2));
+// console.log(typeof(balance.toString()));
+// console.log(balance.toFixed(2));
 
-const newnumber=95.656878
-console.log(newnumber.toPrecision(5));
+// const newnumber=95.656878
+// console.log(newnumber.toPrecision(5));
+
+// const zeroscount=1200100000
+// console.log(zeroscount.toLocaleString('en-IN'));
+
+//++ math
+
+console.log(Math)
+console.log(Math.abs(-128))
+console.log(Math.round(-128.6))
+console.log(Math.ceil(-128.2))
+console.log(Math.floor(-128.9))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log((Math.random()*10 )+1)
+
+console.log((Math.floor(Math.random()*10 )+1))
+
+const max=50;
+const min=10;
+
+console.log(Math.random() *(max-min+1) +min)
+
