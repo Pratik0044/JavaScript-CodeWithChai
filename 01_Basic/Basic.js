70605734c59fc9dc4358c0413fba9ac39ea51a6c
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-01-21 08:27:56 +0000

    update

--- 01_Basic/Basic.js
+++ 01_Basic/Basic.js
@@ -1,1 +1,8 @@
 console.log("Pratik")
+const acountId=151515
+let acountEmail="abc@gmail.com"
+var acountCity="lucknow"
+acountState="Ram"
+console.log(acountId)
+
+console.table([acountId,acountEmail,acountCity,acountState])
