93074d28daf9fc8a158e0a16c4272014e9170c07
PratikVishwakarma <pratikvishwakarma006@gmail.com>
2024-01-22 22:34:05 +0530

    update

---
+++ 01_Basic/05_array.js
@@ -0,0 +1,43 @@
+const arr=[100,500,200,100,150]
+const arr2=["Pratik","Suraj",500,"Lucknow"]
+
+// console.log(arr[3]);
+// console.log(arr2[1]);
+
+// Methods
+
+arr.push(400)
+arr.push(500)
+arr.pop();
+// console.log(arr);
+
+arr.unshift(222)
+// console.log(arr);
+arr.shift()
+
+// console.log(arr);
+
+// console.log(arr.includes(500))
+// console.log(arr.indexOf(500));
+
+let newArray=arr.join()
+// console.log(typeof(arr));
+// console.log(typeof(newArray));
+
+console.log("A", arr)
+let a1=arr.slice(1,3);
+console.log(a1);
+console.log("B", arr);
+let a2=arr.splice(1,3)
+console.log("c",arr);
+console.log(a2);
+
+
+
+
+
+
+
+
+
+
