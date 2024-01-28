26aed525c70a23ee1b9a949d734944e7ddac3a6d
Pratik Vishwakarma <pratikvishwakarma006@gmail.com>
2024-01-28 14:39:15 +0530

    update

--- 02_Functions/02_functions2.js
+++ 02_Functions/02_functions2.js
@@ -1,19 +1,21 @@
 function calculateCartPrice(num1){
     return num1;
 }
-console.log(calculateCartPrice(200,300,400));//only single argument is return from funcion
+
+// console.log(calculateCartPrice(200,300,400));//only single argument is return from funcion
 
 // use REST(...) parameter to return undefined numbers of parameter
 
 function calculateCartPrice2(...num1){
     return num1;
 }
-console.log(calculateCartPrice2(200,300,400));
+
+// console.log(calculateCartPrice2(200,300,400));
 
 function calculateCartPrice3(val1,val2,...num1){
     return num1;
 }
-console.log(calculateCartPrice3(200,300,400,555,666,999));
+// console.log(calculateCartPrice3(200,300,400,555,666,999));
 
 // passing an object to a function
 
@@ -26,5 +28,13 @@ const user={
 function handleObject(anyobject){
     console.log(`Username is ${anyobject.name} and age is ${anyobject.age}`);
 }
-handleObject(user)
+
+// handleObject(user)
+
 // we can pass an array as an argument 
+var array=[2,6,9,7,5,2];
+function findSecondElement(arr){
+    return arr[1];
+}
+console.log(findSecondElement(array));
+
