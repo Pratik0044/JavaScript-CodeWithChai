48c15cc2c9de395745fc94c7d91a44c77fbc5c27
Pratik Vishwakarma <pratikvishwakarma006@gmail.com>
2024-02-08 18:32:56 +0530

    Update

--- 02_Functions/04_arrow_functions.js
+++ 02_Functions/04_arrow_functions.js
@@ -1,8 +1,8 @@
-const user={
+const user = {
     username: "Pratik",
     price: 900,
-    
-    welcomMsg: function(){
+
+    welcomMsg: function () {
         console.log(`${this.username} Welcome to website`);// THIS is a current context
         console.log(this);
     }
@@ -17,8 +17,43 @@ const user={
 console.log(this)//jab ham node invironment me hote hai to this {} print karta hai 
 // jise empty object bolate hai
 
-function chai(){
-    console.log(this);
+// function chai(){
+//     const user="pratik"
+//     console.log(this.user);// THIS kuchh to hai jise kewal object me use kar sakate hai 
+// }
+
+// chai()
+
+//++++++++++++Another way to declear an function ++++++++++++
+const chai = function () {
+    console.log("Fucntion calling");
+}
+// chai()
+
+//?????????????  ARROW FUNCTION   ????????????
+
+const arrowChai = () => {
+    console.log("Arrow Fucntion is calling")
 }
 
-chai()
+arrowChai()
+
+const arrowFun = (n1,n2)=>{
+    return n1+n2 //ham bina return ke bhi arrow function bana sakate hai
+}
+console.log(arrowFun(3,5));
+
+// const arrowFun2=(n1,n2)=> n1+n2 
+
+//bina { } bhi arrow function likha jata hai magar esaka matalab hot hai ki value return karana hai
+
+// console.log(arrowFun2(8,7));
+
+
+//------------ ya ek aur tarika hai ------------
+const arrowFun2=(n1,n2)=> (n1+n2) //ye tarika jyada sahi hota hai kyoki object return karane me pahala wala fail ho jata hai
+console.log(arrowFun2(8,0));
+
+const arrowFucntion=()=>({name: "pratik"})
+
+console.log(arrowFucntion());
