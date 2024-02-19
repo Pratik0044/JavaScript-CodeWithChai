ad1d7b0b1d695b0e331b8e4937e5a8dbde45eecc
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-02-19 09:21:05 +0530

    Update in Project 1

--- 06_Projects/01_Project1/script.js
+++ 06_Projects/01_Project1/script.js
@@ -6,20 +6,25 @@ buttons.forEach(function(bt){
     bt.addEventListener('click',function(e){
         console.log(e);
         console.log(e.target);
-        if(e.target.id==='grey'){
+        if(e.target.id==='Grey'){
             body.style.backgroundColor = e.target.id;
+            alert(`Your Bakgroud IMAGE has to be change as: ${e.target.id}`)
         }
-        if(e.target.id==='white'){
+        if(e.target.id==='White'){
             body.style.backgroundColor = e.target.id;
+            alert(`Request to change background Color as :-> ${e.target.id}`)
         }
-        if(e.target.id==='blue'){
+        if(e.target.id==='Blue'){
             body.style.backgroundColor = e.target.id;
+            alert(`Request to change background Color as :-> ${e.target.id}`)
         }
-        if(e.target.id==='yellow'){
+        if(e.target.id==='Yellow'){
             body.style.backgroundColor = e.target.id;
+            alert(`Request to change background Color as :-> ${e.target.id}`)
         }
-        if(e.target.id==='red'){
+        if(e.target.id==='Red'){
             body.style.backgroundColor = e.target.id;
+            alert(`Request to change background Color as :-> ${e.target.id}`)
         }
     })
 });
