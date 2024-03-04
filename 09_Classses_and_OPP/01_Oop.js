c8a33590336c43ff1984a8f25d6083906b48b6ab
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-03-04 22:15:13 +0530

    Update

--- 09_Classses_and_OPP/01_Oop.js
+++ 09_Classses_and_OPP/01_Oop.js
@@ -1,10 +1,25 @@
-const user = {
-    username: "Pratik",
-    loginCount: 10,
-    signedIn: true,
+// const user = {
+//     username: "Pratik",
+//     loginCount: 10,
+//     signedIn: true,
 
-    getUserDetails: function(){
-        console.log("Got User details from DataBase.");
+//     getUserDetails: function(){
+//         console.log("Got User details from DataBase.");
+//     }
+// }
+
+function user(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
     }
+    return this;
 }
 
+const userOne = new user("Pratik", 13, true);
+const userTwo = new user("Suraj", 15, 0);
+
+console.log(userOne.constructor);
